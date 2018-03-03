f5cf1a1442803d07d6c16f080837b417502dc1a6
zyt <ijumper@163.com>
2018-03-03 22:25:12 +0800

    add codes

---
+++ libs/date.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import time
+
+
+def now():
+    return time.time()
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import tushare
+# import pymysql
+# pymysql.install_as_MySQLdb()
+from sqlalchemy import create_engine
+import tushare as ts
+from libs.date import now
 
 
 if __name__ == '__main__':
-    import tushare as ts
+    stock_code = '600846'
+    start = now()
+    df = ts.get_hist_data(stock_code)
+    engine = create_engine('mysql://root:123456@127.0.0.1/stocksky?charset=utf8')
 
-    print(ts.get_hist_data('600848'))  # 一次性获取全部数据
+    # 存入数据库
+    df.to_sql(stock_code + '_hist_data', engine, if_exists='replace')
+
+    print("Time cost {0}".format(now() - start))
+    # 追加数据到现有表
+    # df.to_sql('hist_data',engine,if_exists='append')
